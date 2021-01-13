719b8ab43477bcfa47e5b3a8b0df41474b73eac1
Scott <richardsprins@gmail.com>
2021-01-13 13:13:36 -0800

    trying again

--- app/layouts/Layout.js
+++ app/layouts/Layout.js
@@ -1,5 +1,6 @@
 import { Head } from "blitz"
 
+<<<<<<< HEAD
 const Layout = ({ title, children }) => {
   return (
     <>
@@ -12,5 +13,20 @@ const Layout = ({ title, children }) => {
     </>
   )
 }
+=======
+const Layout = ({
+  title,
+  children
+}) => {
+  return <>
+    <Head>
+      <title>{title || "pumpt-2.0"}</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    {children}
+  </>;
+};
+>>>>>>> ac2843419adee2bc001724452342e46e4eb8c4f4
 
 export default Layout
